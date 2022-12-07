96bc18b8c18c9cd8ecd9839873cb599534ee4773
Dave Tang <davetingpongtang@gmail.com>
2022-12-07 16:49:04 +0900

    Plot views

---
+++ code/plot_view.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) < 1){
+  message("Please provide the the stats.csv file")
+  quit(status = 1)
+}
+
+suppressPackageStartupMessages(library(tidyverse))
+theme_set(theme_minimal())
+
+views <- read_csv(args[1])
+
+views$day <- factor(
+  weekdays(views$date),
+  levels = c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+)
+views$weekend <- grepl(pattern = "^S", x = views$day)
+views$month <- factor(months(views$date), levels = month.name)
+views$quarter <- factor(quarters(views$date))
+views$year <- factor(format(views$date, "%Y"))
+views <- views[-nrow(views), ]
+
+p <- ggplot(slice_tail(views, n = 30), aes(date, views, fill = weekend)) +
+  geom_col() +
+  scale_x_date(date_breaks = "1 day") +
+  scale_fill_manual(values = c("skyblue", "grey")) +
+  coord_flip() +
+  theme(axis.title.y = element_blank()) +
+  ggtitle("Site views in the last 30 days") +
+  NULL
+
+ggsave("../output/views.png", p)
+quit()
